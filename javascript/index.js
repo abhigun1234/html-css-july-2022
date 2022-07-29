1777e9930ce5971b8249e28c790d3ca25a179d00
Abhishek <abhi.ghosh27feb@gmail.com>
2022-07-29 09:17:14 +0530

    add

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+console.log("hello world")
+var a=10;
+console.log("a",a)
+console.log("a",typeof(a))
+var a='hello'
+console.log("a",typeof(a))
+// operators
+
+// arthemetic conditional comperision logical
+
+
+// + - % * **
+
+var a=Number(prompt("enter first no"))
+var b=Number(prompt("enter second no"))
+
+// +
+var result =a+b
+console.log("a+b",a+b)
+
+
+
+
