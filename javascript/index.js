235ab43c069837a986b2266fa7b79d8122e37b92
Abhishek <abhi.ghosh27feb@gmail.com>
2022-08-01 09:27:51 +0530

    add

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,9 @@
-console.log("hello world")
-var a=10;
-console.log("a",a)
-console.log("a",typeof(a))
-var a='hello'
-console.log("a",typeof(a))
+// console.log("hello world")
+// var a=10;
+// console.log("a",a)
+// console.log("a",typeof(a))
+// var a='hello'
+// console.log("a",typeof(a))
 // operators
 
 // arthemetic conditional comperision logical
@@ -11,13 +11,77 @@ console.log("a",typeof(a))
 
 // + - % * **
 
-var a=Number(prompt("enter first no"))
-var b=Number(prompt("enter second no"))
+// var a=Number(prompt("enter first no"))
+// var b=Number(prompt("enter second no"))
 
-// +
-var result =a+b
-console.log("a+b",a+b)
+// // +
+// var result =a+b
+// console.log("a+b",a+b)
+
+// var b=null;
+// // undifined and null
+// console.log("b",typeof(b))
+// var c;
+// console.log("c",typeof(c))
+
+// < > <= >= == === 
+
+
+// x=1
+// y=11
+// z=1
+//== check only data
+// === data and datatype
+// console.log("x>y",x>y)
+// console.log("x<y",x<y)
+// console.log("x>=y",x>=y)
+// console.log("x<=y",x<=y)
+// console.log("x==y",x==y)
+// console.log("x===y",x===y)
+// alert(x===y)
+
+
+// if(x>y){
+
+//     console.log("x is greater")
+// }
+// else if(y>z){
+//     console.log("y is greater") 
+// }
+// else{
+//     console.log("z is greater")
+// }
+
+
+// let es6  var 
 
 
 
+// function hello(){
+//  var a; 
+// console.log( "a",a)
 
+// }
+// hello()
+// console.log( "a",a)
+
+
+// for(var a=0;a<=10;a++){
+
+//     console.log("a",a)
+// }
+// console.log("a",a)
+
+// for(let a=0;a<10;a++){
+
+//     console.log("a",a)
+// }
+// console.log("a",a)
+// const a=12;
+
+// a=13
+var j=10;
+console.log(typeof(j))//string undifined
+var l='10';
+console.log(j==l)//empty objecttrue
+console.log(j===l)//empty object true
